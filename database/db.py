76a340eaa8ec922d62a3b372599c16bdef8cdfc2
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2024-11-24 10:20:44 +0500

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,48 +1,22 @@
-from aiogram import Router, types
-from aiogram.filters import Command
-from sqlalchemy.ext.asyncio import AsyncSession
-from functions.all_func import get_category_keyboard, get_order_history, get_user_info
-# Эҷоди router
-menu_router = Router()
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.future import select
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from database.tables import *
+import os
+from dotenv import load_dotenv
+import asyncmy
 
-# Функсияи "Меню" барои нишон додани клавиатураи категорияҳо
-@menu_router.message(Command(commands=["menu"]))
-async def menu_handler(message: types.Message):
-    keyboard = await get_category_keyboard()
-    await message.answer("Категорияҳоро интихоб кунед:", reply_markup=keyboard)
-
-
+load_dotenv()
+db_url=os.getenv("DATABASE_URL")
 
 
+# Асинкронӣ пайвастшавӣ ба MySQL
+engine = create_async_engine(db_url, echo=True)
+SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-
-# Функсияи "Фармоишот" барои нишон додани таърихи фармоишҳо
-@menu_router.message(Command(commands=["orders"]))
-async def orders_handler(message: types.Message):
-    session = SessionLocal()
-    user_id = message.from_user.id
-
-    # Гирифтани таърихи фармоишҳо
-    orders = await get_order_history(user_id, session)
-    
-    if not orders:
-        await message.answer("Шумо ҳоло фармоиш надоред.")
-        return
-
-    for order in orders:
-        order_text = f"**Ҳолати фармоиш:** {order['status']}\n\n"
-        for item in order['items']:
-            order_text += (
-                f"📦 **Ном:** {item['name']}\n"
-                f"🔢 **Миқдор:** {item['quantity']}\n"
-                f"💵 **Нарх:** {item['price']} сомонӣ\n"
-                f"💰 **Нархи умумӣ:** {item['total_price']} сомонӣ\n\n"
-            )
-            if item['image_url']:
-                await message.answer_photo(photo=item['image_url'], caption=order_text)
-            else:
-                await message.answer(order_text)
-        
-        order_text = f"💳 **Нархи умумии фармоиш:** {order['total_order_price']} сомонӣ\n"
-    await message.answer(info_text)
-
+async def init_db():
+    async with engine.begin() as conn:
+        # Сохтани ҳамаи таблицаҳо дар база
+        await conn.run_sync(Base.metadata.create_all)

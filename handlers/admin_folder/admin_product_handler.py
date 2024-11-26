c2e742372f4ae7cf32929b7ae7e78bfec2a89295
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2024-11-27 04:46:29 +0500

    Update admin_product_handler.py

--- handlers/admin_folder/admin_product_handler.py
+++ handlers/admin_folder/admin_product_handler.py
@@ -71,7 +71,7 @@ async def delete_product(callback_query: CallbackQuery):
         if product:
             await session.delete(product)
             await session.commit()
-            await callback_query.answer("Маҳсулот ҳазф шуд!")
+            await callback_query.message.edit_text("Маҳсулот ҳазф шуд!")
         else:
             await callback_query.answer("Маҳсулот ёфт нашуд!")
 

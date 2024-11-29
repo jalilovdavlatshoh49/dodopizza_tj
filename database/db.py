6bfdcec8d9d3a994bcd4b6440071e2cc9b65eabf
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2024-11-29 07:11:54 +0500

    Update db.py

--- database/db.py
+++ database/db.py
@@ -71,6 +71,35 @@ class Cart(Base):
         return total_price
 
 
+
+# Модели маҳсулот дар сабад
+class CartItem(Base):
+    __tablename__ = 'cart_item'
+
+    id = Column(Integer, primary_key=True, index=True)
+    product_type = Column(String(255), index=True)  # Навъи маҳсулот
+    product_id = Column(Integer, index=True)  # ID маҳсулот
+    quantity = Column(Integer, default=1)  # Миқдори маҳсулот
+    cart_id = Column(Integer, ForeignKey('cart.id'))  # ID сабад
+    cart = relationship("Cart", back_populates="items")  # Системаи алоқаманд бо Cart
+
+    async def get_price(self) -> float:
+        session = SessionLocal()
+        """Ҳисоби нархи маҳсулот."""
+        product_model = globals().get(self.product_type.capitalize())
+        if product_model:
+            result = await session.execute(select(product_model).filter(product_model.id == self.product_id))
+            product = result.scalars().first()
+            if product:
+                return product.price
+        return 0
+
+    async def get_total_price(self) -> float:
+        session = SessionLocal()
+        """Нархи умумии ин маҳсулот."""
+        price = await self.get_price(session)
+        return price * self.quantity
+
 async def init_db():
     async with engine.begin() as conn:
         # Сохтани ҳамаи таблицаҳо дар база

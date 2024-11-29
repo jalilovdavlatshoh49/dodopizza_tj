442a9f7a23c588b6470d55549e1c8d958b3e3ef3
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2024-11-29 08:17:24 +0500

    Update tables.py

--- database/tables.py
+++ database/tables.py
@@ -104,37 +104,7 @@ class Product(Base):
 
 
 
-class Order(Base):
-    __tablename__ = 'order'
 
-    id = Column(Integer, primary_key=True, index=True)
-    cart_id = Column(Integer, ForeignKey('cart.id'))  # Равиш ба сабад
-    cart = relationship("Cart", back_populates="order")
-    
-    # Ҳолати заказ
-    status = Column(Enum(OrderStatus), default=OrderStatus.PENDING)
-    
-    # Маъзарат ва маълумоти рақами телефон
-    customer_name = Column(String(255), index=True)  # Номи мизоҷ
-    phone_number = Column(String(15), index=True)    # Рақами телефон (String барои +)
-
-    # Барои пайгирии id-и Telegram
-    user_id = Column(BigInteger, index=True)  # Telegram User ID
-
-    # Маълумоти суроға
-    address = Column(String(512), nullable=True)  # Суроғаи дастӣ
-    latitude = Column(Float, nullable=True)       # Координатаҳои GPS
-    longitude = Column(Float, nullable=True)
-
-    def __init__(self, cart, customer_name, phone_number, user_id, address=None, latitude=None, longitude=None):
-        self.cart = cart
-        self.customer_name = customer_name
-        self.phone_number = phone_number
-        self.user_id = user_id
-        self.status = OrderStatus.PENDING
-        self.address = address
-        self.latitude = latitude
-        self.longitude = longitude
 
 
 

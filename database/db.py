c55a455cdd1abdabde757000b9f5f8fb88e8695a
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2024-12-26 12:43:28 +0500

    Update db.py

--- database/db.py
+++ database/db.py
@@ -30,7 +30,7 @@ class Order(Base):
     id = Column(Integer, primary_key=True, index=True)
     cart_id = Column(Integer, ForeignKey('cart.id'))  # Равиш ба сабад
     cart = relationship("Cart", back_populates="order")
-    total_price = Column(Float, nullable=False)  # Ensure this field exists
+    
     status = Column(Enum(OrderStatus), default=OrderStatus.PENDING)
 
     customer_name = Column(String(255), index=True)  
@@ -42,12 +42,11 @@ class Order(Base):
     latitude = Column(Float, nullable=True)       
     longitude = Column(Float, nullable=True)
 
-    def __init__(self, cart, customer_name, phone_number, user_id, total_price, address=None, latitude=None, longitude=None):
+    def __init__(self, cart, customer_name, phone_number, user_id, address=None, latitude=None, longitude=None):
         self.cart = cart
         self.customer_name = customer_name
         self.phone_number = phone_number
         self.user_id = user_id
-        self.total_price = total_price
         self.status = OrderStatus.PENDING
         self.address = address
         self.latitude = latitude

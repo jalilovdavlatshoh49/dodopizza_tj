2859b0c94593496a9cfa0d55bd51cdbcf4113943
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2024-12-26 18:16:53 +0500

    Update admin_accept.py

--- handlers/admin_folder/admin_accept.py
+++ handlers/admin_folder/admin_accept.py
@@ -2,7 +2,7 @@ from aiogram import Router, types
 from sqlalchemy.future import select
 from database.tables import OrderStatus  # Ба модели худ истинод кунед
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
-from database.db import Cart, SessionLocal
+from database.db import Cart, Order, SessionLocal
 admin_accept = Router()
 
 
